431e9c399155fce0647068a941016824f0b945c5
ryukikikie <ryukikikie@gmail.com>
2020-08-10 16:54:43 +0900

    Complete login using oauth1 oob

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"github.com/gomodule/oauth1/oauth"
+)
+
+var oauthClient = oauth.Client{
+	TemporaryCredentialRequestURI: "https://api.twitter.com/oauth/request_token",
+	ResourceOwnerAuthorizationURI: "https://api.twitter.com/oauth/authorize",
+	TokenRequestURI:               "https://api.twitter.com/oauth/access_token",
+}
+
+var credPath = flag.String("config", "config.json", "Path to configuration file containing the application's credentials.")
+
+func readCredentials() error {
+	b, err := ioutil.ReadFile(*credPath)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, &oauthClient.Credentials)
+}
+
+func main() {
+	fmt.Println("hello")
+	// https://developer.twitter.com/en/docs/basics/authentication/api-reference/request_token
+
+	// Obtaining a request token
+	if err := readCredentials(); err != nil {
+		log.Fatal(err)
+	}
+
+	tempCred, err := oauthClient.RequestTemporaryCredentials(nil, "oob", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	u := oauthClient.AuthorizationURL(tempCred, nil)
+
+	fmt.Printf("1. Go to %s\n2. Authorize the application\n3. Enter verification code:\n", u)
+	openbrowser(u)
+
+	var code string
+	fmt.Scanln(&code)
+
+	tokenCred, _, err := oauthClient.RequestToken(nil, tempCred, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := oauthClient.Get(nil, tokenCred,
+		"https://api.twitter.com/1.1/statuses/home_timeline.json", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
